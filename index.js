9fe433ec2644b3a42b2e184970b4a1dfb8b7e4b6
FelipePelissoli <felipe.pelissoli@gmail.com>
2023-09-15 01:51:52 -0300

    atualização

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-console.log("teste")
+
+let nome = "NomeDoHerói"
+let xp = 500
+let nivel
+
+switch (xp) {
+    case xp > 1 && xp <= 1000:
+    nivel = "Bronze"
+    break
+
+    case xp > 1001 && xp <= 2000:
+    nivel = "Prata"
+    break
+
+    case xp > 2001 && xp <= 5000:
+    nivel = "Ouro"
+    break
+
+    case xp > 5001 && xp <= 7000:
+    nivel = "Platina"
+    break
+
+    case xp > 7001 && xp <= 9000:
+    nivel = "Ascendente"
+    break
+
+    case xp > 9001 && xp <= 10000:
+    nivel = "Imortal"
+    break
+
+    case xp >= 10001:
+    nivel = "Radiante"
+    break
+}
+
+console.log("O Herói de nome " + nome + " está no nível de " + nivel)
+
+
